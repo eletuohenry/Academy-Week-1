c05da82235089ac4843e1268a2b6b9d31fd18e75
Henry Eletuo <eletuohenry@gmail.com>
2023-01-29 15:38:16 -0500

    First commit

---
+++ Tutorials/src/Tutorials/Main.java
@@ -0,0 +1,18 @@
+package Tutorials;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		x = 100;
+		int y;
+		y = 36;
+		int sum = x * y;
+		System.out.println(sum);
+		String str = "Hello Henry, Welcome to Our World!";
+		System.out.println(str);
+
+	}
+
+}
