3e6884577e3413e24b12be9c8e2d48fc2e66b9e7
eletuohenry <47346267+eletuohenry@users.noreply.github.com>
2023-01-29 16:28:33 -0500

    Add files via upload

---
+++ numbers_operations.py
@@ -0,0 +1,140 @@
+# This is me learning math operations in python
+import keyword
+
+exponents = 6 ** 7
+print (exponents)
+
+exponent = 6**7
+print (exponent)
+
+a = 6
+b = 7
+add = a + b
+sub = b - a
+multi = a * b
+div = b / a
+exponents = a ** b
+
+print (add)
+print (sub)
+print (multi)
+print (div)
+print (exponents)
+
+a = 50
+a += 50
+print (a)
+
+a = 20
+b = 30
+c = a + b * 2
+print (c)
+
+c /= 4
+print(c)
+
+# Arithmetic order of precedence
+
+a = 2 + 6 * 7 / 3
+b = 3 * 5 ** 4 + 6
+print (a + b)
+print (a)
+print (b)
+
+q = 714
+r = 2000
+add = q + r
+sub = r - q
+div = r / q
+multi = r * q
+exponent = r ** q
+
+print(add)
+print(sub)
+print(div)
+print(multi)
+print(exponent)
+
+a = 20
+a = a + 50
+print(a)
+a += 50
+print(a)
+
+x = 19
+y = 21
+z = x + y
+print(z)
+h = z * 3
+print(h)
+l = 32
+u = 98
+p = 56
+add = l + u + p
+print (add)
+sub = u - p - l
+print(sub)
+print(l+100)
+o = 10
+o = o + 60
+print(o)
+div = l / p * l
+multi = l * u * p
+print(div)
+print(multi)
+exponent = 12 ** 2
+print(exponent)
+
+i = 20
+o = 60
+u = 17
+add = i + o + u
+print(add)
+div1 = o / u
+print(div1)
+exponent_new = o * u
+print(exponent_new)
+print("%$"*20)
+print(((1+2)*3)/4)
+
+
+f = 4 ** 5
+print(4**5)
+print(((1+2)*3)/4)
+print(2.25**5)
+
+2+2 == 4
+
+amount = 47.28
+tip = 47.28*0.15
+total = (47.28 + 47.28*0.15)
+each = (47.28 + 47.28*0.15)/2
+print("Each person needs to pay:", (each))
+
+div = 2/2
+print(div)
+print("$$$$%##^^^&&&&*")
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+    print("Feel free to ask questions about " + department)
+
+greeting("Toby", "IT Support")
+greeting("Chidera", "Accounts")
+
+patient_name = "John Smith"
+patient_age = 20
+
+rem = 10 % 3
+print(rem)
+
+v = 30
+print(v)
+v *= 40
+print(v)
+
+
+
+
+
